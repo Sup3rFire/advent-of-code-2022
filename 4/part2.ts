572be218d72d1bab4e672cacbeb80bd322dcbee4
Super Fire <36259493+Sup3rFire@users.noreply.github.com>
2022-12-04 20:02:20 +0700

    Day 4 part2

---
+++ 4/part2.ts
@@ -0,0 +1,14 @@
+const rawInput = Deno.readTextFileSync("input.txt").trim();
+
+const fullyContains = rawInput
+  .split("\n")
+  .map((pair) =>
+    pair.split(",").map((person) => person.split("-").map((value) => +value))
+  )
+  .filter((pair) =>
+    pair[1][0] < pair[0][0]
+      ? pair[1][1] >= pair[0][0]
+      : pair[0][1] >= pair[1][0]
+  );
+
+console.log(fullyContains.length);
